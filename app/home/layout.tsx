21b6de94ce72f6534e93a3b3ae32487439127e16
Arthur Papailhau <papailhau@gmail.com>
2025-08-02 01:50:18 -0700

    Fixes header in /home

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -8,7 +8,9 @@ export default function HomeLayout({
   return (
     <div className="h-full bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       <Header />
-      {children}
+      <main className="pt-14">
+        {children}
+      </main>
     </div>
   );
 }
